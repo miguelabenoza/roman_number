063f200d37829d92bcb21070319680994976ed75
miguel_abenoza <miguel.abenoza@icloud.com>
2024-07-01 21:03:42 +0200

    tests con diccionario

--- roman_funcs.py
+++ roman_funcs.py
@@ -10,20 +10,6 @@ values = {
 }
 
 def to_roman(n):
-
-    if n == 5:
-        result = 'V'
-    elif n == 1:
-        result = 'I'
-    elif n == 10:
-        result = 'X'
-    elif n == 50:
-        result = 'L'
-    elif n == 100:
-        result = 'C'
-    elif n == 500:
-        result = 'D'
-    elif n == 1000:
-        result = 'M'
+    result = values[n]
     
     return result
